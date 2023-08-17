6de574f5038396aec53ae332610e8a09e7ad3b6e
devmikh <mishacyb@gmail.com>
2023-08-17 11:03:11 -0700

    Structure routes, add CORS middleware

--- app.ts
+++ app.ts
@@ -1,4 +1,7 @@
 import express from 'express'
+import cors from 'cors'
+
+import router from './src/routes'
 
 // Load env variables
 import dotenv from 'dotenv'
@@ -6,8 +9,17 @@ dotenv.config()
 
 const app = express()
 
+// Connect middleware
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+// Connect router
+app.use(router)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`)
